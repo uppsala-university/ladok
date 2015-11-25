cc2810c4e2b890a000a9840ab228be5de0e34da3
markus <markus@jardemalm.se>
2015-11-25 15:49:43 +0100

    Added new service and implemented first call.

---
+++ ladok3rest-client/src/main/java/se/sunet/ati/ladok/rest/dto/utbildningsinformation/Utbildningstillfalle.java
@@ -0,0 +1,28 @@
+package se.sunet.ati.ladok.rest.dto.utbildningsinformation;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import se.sunet.ati.ladok.rest.dto.studiedeltagande.Link;
+
+@XmlRootElement(name = "Utbildningstillfalle", namespace = "http://schemas.ladok.se/utbildningsinformation")
+public class Utbildningstillfalle implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@XmlElement(name = "Uid", namespace = "http://schemas.ladok.se")
+	String uid;
+	
+//	@XmlElement(name = "link", namespace = "http://schemas.ladok.se/dap")  
+//	List<Link> links;	
+	
+	@XmlElement(name = "Tillfalleskod", namespace = "http://schemas.ladok.se")
+	String tillfalleskod;
+
+	public String getUid() {
+		return uid;
+	}
+}
--- ladok3rest-client/src/main/java/se/sunet/ati/ladok/rest/services/Utbildningsinformation.java
+++ ladok3rest-client/src/main/java/se/sunet/ati/ladok/rest/services/Utbildningsinformation.java
@@ -1,5 +1,9 @@
 package se.sunet.ati.ladok.rest.services;
 
+import se.sunet.ati.ladok.rest.dto.utbildningsinformation.Utbildningstillfalle;
+
 public interface Utbildningsinformation {
 
+	public Utbildningstillfalle hamtaUtbildningstillfalleViaUtbildningsUtbildningstillfalleUID(String utbildningstillfalleUID);
+	
 }
