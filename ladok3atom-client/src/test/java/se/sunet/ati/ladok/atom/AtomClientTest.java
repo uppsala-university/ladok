0823cfe265202858fcdec71581a87ba45c1f43c7
markus <markus@jardemalm.se>
2015-12-07 15:05:58 +0100

    Added test for pulling ATOM from a given id.

--- ladok3atom-client/src/test/java/se/sunet/ati/ladok/atom/AtomClientTest.java
+++ ladok3atom-client/src/test/java/se/sunet/ati/ladok/atom/AtomClientTest.java
@@ -1,5 +1,7 @@
 package se.sunet.ati.ladok.atom;
 
+import static org.junit.Assert.*;
+
 import java.util.List;
 
 import org.apache.abdera.model.Entry;
@@ -21,6 +23,28 @@ public class AtomClientTest {
 		} else {
 			System.out.println("No entries found.");
 		}
+	}
+	
+	@Test
+	public void testAtomClientGetAllEntriesFromGivenId() throws Exception {
+		
+		String lastReadEntryId = "https://api.mit.ladok.se/uppfoljning/feed/2;10212d7e-9c73-11e5-865c-257de21b4421";
+
+		AtomClient ac = new AtomClient();
+		
+		List<Entry> entries = ac.getEntries(lastReadEntryId);
+
+		if (entries != null) {
+			System.out.println("Found " + entries.size() + " entries");
+			for (Entry e : entries) {
+				System.out.println("entryid: " + e.getId() + ", baseuri: " + e.getBaseUri());
+			}
+		} else {
+			System.out.println("No entries found.");
+		}
+		
+		assertTrue(!entries.isEmpty());
+		
 	}
 
 }
